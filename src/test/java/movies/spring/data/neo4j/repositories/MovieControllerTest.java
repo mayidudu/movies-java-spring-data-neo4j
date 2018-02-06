b7e6ecce9e97860ca630d47a45aa676fd8df26c8
mawentao <316148732@qq.com>
2018-02-06 16:01:48 +0800

    test

--- src/test/java/movies/spring/data/neo4j/repositories/MovieControllerTest.java
+++ src/test/java/movies/spring/data/neo4j/repositories/MovieControllerTest.java
@@ -39,16 +39,11 @@ public class MovieControllerTest {
         Movie matrix = new Movie("The Matrix", 1999);
         instance.save(matrix);
 
-
-
         Person keanu = new Person("Keanu Reeves");
         personRepository.save(keanu);
 
-
         Role neo = new Role(matrix, keanu);
         neo.addRoleName("Neo");
-
-
 
         matrix.addRole(neo);
         instance.save(matrix);
