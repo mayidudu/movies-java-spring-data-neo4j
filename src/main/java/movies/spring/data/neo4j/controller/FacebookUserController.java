f8709b8ef69cd27c585a8a734156d15c59f64f05
mawentao <316148732@qq.com>
2018-02-07 16:39:15 +0800

    new create interface

--- src/main/java/movies/spring/data/neo4j/controller/FacebookUserController.java
+++ src/main/java/movies/spring/data/neo4j/controller/FacebookUserController.java
@@ -1,18 +1,25 @@
 package movies.spring.data.neo4j.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import movies.spring.data.neo4j.domain.FacebookUser;
+import movies.spring.data.neo4j.repositories.FacebookUserRepository;
 import movies.spring.data.neo4j.services.FacebookService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by marvin on 2018/2/6.
  */
-@RestController("/facebook")
+@RestController
+@RequestMapping("/api/v1/facebook")
 public class FacebookUserController {
     final FacebookService facebookService;
+
+    @Autowired
+    private FacebookUserRepository facebookUserRepository;
 
     public FacebookUserController(FacebookService facebookService){
         this.facebookService = facebookService;
@@ -21,5 +28,19 @@ public class FacebookUserController {
     @RequestMapping("/fbgraph")
     public Map<String, Object> graph(@RequestParam(value = "limit",required = false) Integer limit) {
         return facebookService.graph(limit == null ? 100 : limit);
+    }
+
+    @RequestMapping(value = "/create",method = RequestMethod.POST)
+    public @ResponseBody
+    JSONObject create(@RequestBody JSONObject data) throws Exception{
+        System.out.println("create ========");
+        FacebookUser facebookUser = new FacebookUser();
+        facebookUser.setPathname("pathname");
+        facebookUser.setScreenname(data.getString("screenname"));
+        facebookUser.setUid(1000l);
+        facebookUserRepository.save(facebookUser);
+        JSONObject res = new JSONObject();
+        res.put("error",1);
+        return res;
     }
 }
--- src/main/java/movies/spring/data/neo4j/controller/MovieController.java
+++ src/main/java/movies/spring/data/neo4j/controller/MovieController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author Mark Angrish
  */
-@RestController("/")
+@RestController("/movie")
 public class MovieController {
 
 	final MovieService movieService;
