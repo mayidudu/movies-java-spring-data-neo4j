20401f013a1189a66210d9c5ea2462f296e5915e
mawentao <316148732@qq.com>
2018-02-07 18:49:32 +0800

    vertx

--- src/main/java/com/jn/rb/data/process/DataProcessApp.java
+++ src/main/java/com/jn/rb/data/process/DataProcessApp.java
@@ -1,8 +1,13 @@
 package com.jn.rb.data.process;
 
+import com.jn.rb.data.process.config.SpringConfiguration;
+import com.jn.rb.data.process.vertx.FacebookVerticle;
+import io.vertx.core.Vertx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * @author Michael Hunger
@@ -13,6 +18,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 public class DataProcessApp {
 
 	public static void main(String[] args) {
+		//可以从获取context
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+		final Vertx vertx = Vertx.vertx();
+		vertx.deployVerticle(new FacebookVerticle(context));
+		System.out.println("Deployment done");
 		SpringApplication.run(DataProcessApp.class, args);
 	}
 }
